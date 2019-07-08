d5871c90ac0f94b9c8503482448e909f07c6d66e
Mason Fisher <sackzombie10@gmail.com>
2019-07-07 23:55:35 -0400

    Working on the core

--- src/blockify.py
+++
@@ -1,1 +0,0 @@
-# I "blockify" the image!
--- src/core.py
+++ src/core.py
@@ -1,1 +1,10 @@
 # I'm just the one that executes the instructions!
+from PIL import Image as pillow
+import sys
+
+im = pillow.open(sys.argv[1])
+imwid, imhei = im.size
+
+maxheight = 200
+im.thumbnail((200, maxheight), pillow.ANTIALIAS)
+im.save("oof.JPG")
