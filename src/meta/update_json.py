17d61166f2d7c470edb60e98ed3e964e41ed43b3
Mason Fisher <sackzombie10@gmail.com>
2019-07-07 23:22:57 -0400

    Made the json file updater and updated json file

--- src/meta/update_json.py
+++ src/meta/update_json.py
@@ -1,12 +1,16 @@
 from image_to_pixel import *
-import glob, operator
+import glob, operator, json
 flammable_tex = glob.glob("mc_blocks/flammable/*.png")
 unflammable_tex = glob.glob("mc_blocks/unflammable/*.png")
 all_tex = []
+json_file = open("../blocks.json", "w")
 
 for i in range(len(flammable_tex)):
-    flammable_tex[i] = all_tex.append([flammable_tex[i][20:], True, calc_avg_pixel(flammable_tex[i])])
+    flammable_tex[i] = all_tex.append({"Name":flammable_tex[i][20:], "Flammable?":True, "Color":calc_avg_pixel(flammable_tex[i])})
 for i in range(len(unflammable_tex)):
-    unflammable_tex[i] = all_tex.append([unflammable_tex[i][22:], False, calc_avg_pixel(unflammable_tex[i])])
+    unflammable_tex[i] = all_tex.append({"Name":unflammable_tex[i][22:], "Flammable?":False, "Color":calc_avg_pixel(unflammable_tex[i])})
 
-print(sorted(all_tex, key = operator.itemgetter(0)))
+flist = sorted(all_tex, key = operator.itemgetter("Name"))
+json_file.write(json.dumps(flist, indent=2, sort_keys=True))
+json_file.truncate() # Make JSON file the size of the given text
+json_file.close()
